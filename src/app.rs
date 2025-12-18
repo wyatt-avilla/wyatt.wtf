0c85333ff5f98d54660cf5afd340921eadf41ff7
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-12-17 21:00:52 -0800

    rm counter

--- src/app.rs
+++ src/app.rs
@@ -51,12 +51,7 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
-
     view! {
         <h1>"under construction rn"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
